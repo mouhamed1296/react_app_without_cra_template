8e4a59905e63c188cc6a0518c7aa100f04bcd31e
mamadou sarr <mbayelel@gmail.com>
2021-11-05 20:22:17 +0100

    Test of sard-form validation rule helper function for react-hook-form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './app.css'
-import { Form, FormElement } from 'sard-form'
+import { Form, FormElement, VRules } from 'sard-form'
+import { useForm } from 'react-hook-form'
 
 const App = () => {
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const data = new FormData(e.target)
-    console.log(Object.fromEntries(data.entries()));
+  const { handleSubmit, register, formState: {errors} } = useForm({mode: "onBlur"})
+  const { Required, Email, MinLength, String } = VRules()
+  console.log(errors);
+
+  const onSubmit = (data) => {
+    console.log(data);
   }
 
   return (
     <div className="app">
-     <Form onSubmit={handleSubmit} className="form">
+     <Form onSubmit={handleSubmit(onSubmit)} className="form">
        <FormElement
         type="text"
         name="username"
@@ -25,24 +28,63 @@ const App = () => {
           in: "div",
           className: "form-group"
         }}
+
+        {
+          ...register(
+            "username",
+            {
+              required: Required('Ce champ est requis'),
+              minLength: MinLength('Ce champ doit comporter au moins 2 caractères', 2)
+            }
+          )
+        }
+
       />
       <FormElement
         type="email"
         name="email"
         placeholder="Email"
         className="form-input"
+        {
+          ...register(
+            "email",
+            {
+              required: Required('Ce champ est requis'),
+              pattern: Email("Invalide mail adress")
+            }
+          )
+        }
       />
       <FormElement
         type="text"
         name="fullname"
         placeholder="Full Name"
         className="form-input"
+        {
+          ...register(
+            "fullname",
+            {
+              required: Required('Ce champ est requis'),
+              pattern: String("Ce champ n'accepte que des lettre alphabetiques et des espaces", true),
+              minLength: MinLength('Ce champ doit comporter au moins 3 caractères', 3)
+            }
+          )
+        }
       />
       <FormElement
         type="text"
         name="job"
         placeholder="Job Title"
         className="form-input"
+        {
+          ...register(
+            "job",
+            {
+              required: Required('Ce champ est requis'),
+              minLength: MinLength('Ce champ doit comporter au moins 5 caractères', 5)
+            }
+          )
+        }
       />
       <FormElement type="button" className="btn">Submit</FormElement>
      </Form>
