388321dad206922c73d955b16ff8095869142fd5
mamadou sarr <mbayelel@gmail.com>
2021-11-13 17:05:01 +0100

    extending Object

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './app.css'
-import { Form, FormElement, VRules } from 'sard-form'
+import { _Object } from './Object'
+import { Form, FormElement, VRules} from 'sard-form'
+import { ShowErrorMessage } from './ShowErrorMessage'
 import { useForm } from 'react-hook-form'
 
 const App = () => {
@@ -7,6 +9,104 @@ const App = () => {
   const { handleSubmit, register, formState: {errors} } = useForm({mode: "onBlur"})
   const { Required, Email, MinLength, String } = VRules()
   console.log(errors);
+  const userInfo = {
+    name:"John",
+    age:30,
+    car:null,
+    job: "Developer",
+    address: "Dakar",
+    interest: {
+      music: ["rnb", "rap"],
+      sports: ["football", "basketball"]
+    }
+  }
+  
+  const users = [
+    {
+      name:"John",
+      age:30,
+      car:null,
+      job: "Developer",
+      address: "Dakar",
+      interest: {
+        music: ["rnb", "rap"],
+        sports: ["football", "basketball"]
+      }
+    },
+    {
+      name:"Jane",
+      age:20,
+      car:null,
+      job: "attorney",
+      address: "Dakar",
+      interest: {
+        music: ["rnb", "rap"],
+        sports: ["football", "basketball"]
+      }
+    },
+    {
+      name:"Moussa",
+      age:26,
+      car:null,
+      job: "plice officer",
+      address: "Dakar",
+      interest: {
+        music: ["rnb", "rap", "trap"],
+        sports: ["basketball", "handball"]
+      }
+    }
+  ]
+  const usersO = {
+    1: {
+      name:"John",
+      age:30,
+      car:null,
+      job: "Developer",
+      address: "Dakar",
+      interest: {
+        music: ["rnb", "rap"],
+        sports: ["football", "basketball"]
+      }
+    },
+    2: {
+      name:"Jane",
+      age:20,
+      car:null,
+      job: "attorney",
+      address: "Dakar",
+      interest: {
+        music: ["rnb", "rap"],
+        sports: ["football", "basketball"]
+      }
+    },
+    3: {
+      name:"Moussa",
+      age:26,
+      car:null,
+      job: "plice officer",
+      address: "Dakar",
+      interest: {
+        music: ["rnb", "rap", "trap"],
+        sports: ["basketball", "handball"]
+      }
+    }
+  }
+
+  console.log(Object.entries(usersO))
+  console.log(_Object.getFirstEntry(userInfo));
+  console.table(_Object.getLastEntry(userInfo));
+  console.log(_Object.getEntry(userInfo, 4));
+  console.log(_Object.getEntries(userInfo, 1, 3));
+  console.log(_Object.isEmpty(userInfo));
+  console.log(_Object.toMap(userInfo));
+  console.log(_Object.filter(userInfo, (info) => `${info}`.includes('D')))
+  console.time("array");
+  (() => { console.log(_Object.filter(users, (user) => user.interest.sports.includes("football")))})()
+  console.timeEnd("array");
+  console.time("objet");
+  (() => { console.log(_Object.filter(usersO, (user) => user.interest.sports.includes("football")))})()
+  console.timeEnd("objet");
+  console.log(_Object.map(userInfo, (info) => info+1))
 
   const onSubmit = (data) => {
     console.log(data);
@@ -21,9 +121,7 @@ const App = () => {
         id="username"
         placeholder="Username"
         className="form-input"
-        label={{
-          content: "Username"
-        }}
+        label="Username"
         wrap={{
           in: "div",
           className: "form-group"
@@ -38,6 +136,13 @@ const App = () => {
             }
           )
         }
+        onError={[
+          errors.username,
+          <ShowErrorMessage
+            message={errors.username && errors.username.message}
+            className="error"
+          />
+        ]}
 
       />
       <FormElement
@@ -54,6 +159,10 @@ const App = () => {
             }
           )
         }
+        onError={[
+          errors.email,
+          <ShowErrorMessage message={errors.email && errors.email.message} className="error" />
+        ]}
       />
       <FormElement
         type="text"
@@ -70,6 +179,10 @@ const App = () => {
             }
           )
         }
+        onError={[
+          errors.fullname,
+          <ShowErrorMessage message={errors.fullname && errors.fullname.message} className="error" />
+        ]}
       />
       <FormElement
         type="text"
@@ -81,10 +194,15 @@ const App = () => {
             "job",
             {
               required: Required('Ce champ est requis'),
+              pattern: String("Ce champ n'accepte que des lettre alphabetiques et des espaces", true),
               minLength: MinLength('Ce champ doit comporter au moins 5 caractères', 5)
             }
           )
         }
+        onError={[
+          errors.job,
+          <ShowErrorMessage message={errors.job && errors.job.message} className="error" />
+        ]}
       />
       <FormElement type="button" className="btn">Submit</FormElement>
      </Form>
---
+++ src/ShowErrorMessage.js
@@ -0,0 +1,17 @@
+import { _Object } from './Object'
+
+export const ShowErrorMessage = ({message, className, style, wrap={}}) => {
+    const Wrapper = `${wrap.in}`
+    return (
+        _Object.isEmpty(wrap) ? <span role="alert" className={className}>
+            {message}
+        </span> : <Wrapper className={wrap.className} style={wrap.style}>
+            <span role="alert" className={className} style={style}>
+                {message}
+            </span>
+            <>
+             {wrap.children}
+            </>
+        </Wrapper>
+    )
+}
